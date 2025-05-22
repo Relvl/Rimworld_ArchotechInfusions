2cd953a244888b398a18c6b5f789f643bbbba4f2
Johnson <johnssoft@ya.ru>
2025-05-23 01:29:32 +0500

    'nop' instruction

--- Source/statprocessor/InstructionGeneratorNop.cs
+++ Source/statprocessor/InstructionGeneratorNop.cs
@@ -1,14 +1,17 @@
-﻿using ArchotechInfusions.instructions;
+﻿using System.Diagnostics.CodeAnalysis;
+using ArchotechInfusions.instructions;
+using Verse;
 
 namespace ArchotechInfusions;
 
+[SuppressMessage("ReSharper", "UnusedType.Global")]
 public class InstructionGeneratorNop(StatDefinitionDef def) : AInstructionGenerator<InstructionNop>(def)
 {
     public override string Name => "NOP";
 
     protected override InstructionNop InstantiateInstruction()
     {
-        return new InstructionNop(Def, Def.OperationSet);
+        return new InstructionNop(Def, Operations.RandomElementByWeight(i => i.Weight));
     }
 
     protected override float GenerateValue(AInstruction instruction)
